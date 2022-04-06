ac9ea850f94555f37cb48df4f68c9bf144c30602
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-04-06 12:18:55 -0300

    desafio das matrize refatorando

--- DesafiosTreinaDev/matrix-mixer/lib/matrix_mixer.rb
+++ DesafiosTreinaDev/matrix-mixer/lib/matrix_mixer.rb
@@ -1,113 +1,93 @@
 class MatrixMixer
-  def impares(vetor)
+  def vetor_aux_ordenado_lines(vetor, impares_or_pares_columna)
     array = []
-    vetor.each do |val|
-      array << val  if val % 2 != 0 && val.class == Integer
+    if impares_or_pares_columna.eql?(":ÍMPARES")
+      vetor.each do |val|
+        array << val  if val % 2 != 0 && val.class == Integer
+      end
+    elsif impares_or_pares_columna.eql?(":PARES")
+      vetor.each do |val|
+        array << val if val % 2 == 0 && val.class == Integer
+      end
+    elsif impares_or_pares_columna.eql?(":ALFABETO")
+      vetor.each do |val|
+        array << val if val.class == String
+      end
     end
     return array.sort { |a, b| a <=> b }
   end
 
-  def pares(vetor)
+  def vetores_ordered_lines(vetor, impares_or_pares_columna)
+    array_aux = vetor_aux_ordenado_lines(vetor, impares_or_pares_columna)
+    if impares_or_pares_columna.eql?(":ÍMPARES")
+      vetor.each_index do |index|
+        if vetor[index].class == Integer && vetor[index].odd?
+         v = array_aux.shift
+         vetor[index] = v
+        end
+      end
+    elsif impares_or_pares_columna.eql?(":PARES")
+      vetor.each_index do |index|
+        if vetor[index].class == Integer && vetor[index].even?
+          v = array_aux.shift
+          vetor[index] = v
+        end
+      end
+    elsif impares_or_pares_columna.eql?(":ALFABETO")
+      vetor.each_index do |index|
+        if vetor[index].class == String
+          v = array_aux.shift
+          vetor[index] = v
+        end
+      end
+    end
+  end
+
+  def vetor_aux_ordenado_colum(vetor, column, impares_or_pares_columna)
     array = []
-    vetor.each do |val|
-      array << val if val % 2 == 0 && val.class == Integer
+    if impares_or_pares_columna.eql?(":ÍMPARES")
+      vetor.each_index do |index|
+        array << vetor[index][column]  if vetor[index][column].class == Integer && vetor[index][column].odd?
+      end
+    elsif impares_or_pares_columna.eql?(":PARES")
+      vetor.each_index do |index|
+        array << vetor[index][column]  if vetor[index][column].class == Integer && vetor[index][column].even?
+      end
+    elsif impares_or_pares_columna.eql?(":ALFABETO")
+      vetor.each_index do |index|
+        array << vetor[index][column]  if vetor[index][column].class == String
+      end
     end
     return array.sort { |a, b| a <=> b }
   end
-  
-  def alfabeto(vetor)
-    array = []
-    vetor.each do |val|
-      array << val if val.class == String
-    end
-    return array.sort { |a, b| a <=> b }
-  end
 
-  def ordem_impares(vetor)
-    array_aux = impares(vetor)
-    vetor.each_index do |index|
-      if vetor[index].class == Integer && vetor[index].odd?
-       v = array_aux.shift
-       vetor[index] = v
+  def vetores_ordered_columns(vetor, column, impares_or_pares_columna)
+    array_aux = vetor_aux_ordenado_colum(vetor, column, impares_or_pares_columna)
+    if impares_or_pares_columna.eql?(":ÍMPARES")
+      vetor.each_index do |index|
+        if vetor[index][column].class == Integer && vetor[index][column].odd?
+          v =  array_aux.shift
+          vetor[index][column] = v
+        end
       end
-    end
-  end
-
-  def ordem_pares(vetor)
-    array_aux = pares(vetor)
-    vetor.each_index do |index|
-      if vetor[index].class == Integer && vetor[index].even?
-        v = array_aux.shift
-        vetor[index] = v
+      return vetor
+    elsif impares_or_pares_columna.eql?(":PARES")
+      vetor.each_index do |index|
+        if vetor[index][column].class == Integer && vetor[index][column].even?
+          v =  array_aux.shift
+          vetor[index][column] = v
+        end
       end
-    end
-  end
-
-  def ordem_alfabeto(vetor)
-    array_aux = alfabeto(vetor)
-    vetor.each_index do |index|
-      if vetor[index].class == String
-        v = array_aux.shift
-        vetor[index] = v
+      return vetor
+    elsif impares_or_pares_columna.eql?(":ALFABETO")
+      vetor.each_index do |index|
+        if vetor[index][column].class == String
+          v =  array_aux.shift
+          vetor[index][column] = v
+        end
       end
+      return vetor
     end
-  end
-
-  def ordem_impares_columns(vetor, column)
-    array_columns = []
-    vetor.each_index do |index|
-      array_columns << vetor[index][column]  if vetor[index][column].class == Integer && vetor[index][column].odd?
-    end
-    return array_columns.sort { |a, b| a <=> b }
-  end
-
-  def ordem_colum_impares(vetor, column)
-    array_aux = ordem_impares_columns(vetor, column)
-    vetor.each_index do |index|
-      if vetor[index][column].class == Integer && vetor[index][column].odd?
-        v =  array_aux.shift
-        vetor[index][column] = v
-      end
-    end
-    vetor
-  end
-
-  def ordem_pares_columns(vetor, column)
-    array_columns = []
-    vetor.each_index do |index|
-      array_columns << vetor[index][column]  if vetor[index][column].class == Integer && vetor[index][column].even?
-    end
-    return array_columns.sort { |a, b| a <=> b }
-  end
-
-  def ordem_colum_pares(vetor, column)
-    array_aux = ordem_pares_columns(vetor, column)
-    vetor.each_index do |index|
-      if vetor[index][column].class == Integer && vetor[index][column].even?
-        v =  array_aux.shift
-        vetor[index][column] = v
-      end
-    end
-    vetor
-  end
-
-  def ordem_alfabeto_columns(vetor, column)
-    array_columns = []
-    vetor.each_index do |index|
-      array_columns << vetor[index][column]  if vetor[index][column].class == String
-    end
-    return array_columns.sort { |a, b| a <=> b }
-  end
-
-  def ordem_colum_alfabetos(vetor, column)
-    array_aux = ordem_alfabeto_columns(vetor, column)
-    vetor.each_index do |index|
-      if vetor[index][column].class == String
-        v =  array_aux.shift
-        vetor[index][column] = v
-      end
-    end
-    vetor
   end
 
   def sorter(matrix, instructions)
@@ -115,21 +95,21 @@ class MatrixMixer
       if line.include?("LINHA")
         10.times do |i|
           if line.include?(":ÍMPARES") && line.include?("#{i}")
-            matrix[i] = ordem_impares(matrix[i])
+            matrix[i] = vetores_ordered_lines(matrix[i],":ÍMPARES")
           elsif line.include?(":PARES") && line.include?("#{i}")
-            matrix[i] = ordem_pares(matrix[i])
+            matrix[i] = vetores_ordered_lines(matrix[i],":PARES")
           elsif line.include?(":ALFABETO") && line.include?("#{i}")
-            matrix[i] = ordem_alfabeto(matrix[i])
+            matrix[i] = vetores_ordered_lines(matrix[i],":ALFABETO")
           end
         end
       elsif line.include?("COLUNA")
         10.times do |i|
           if line.include?(":ÍMPARES") && line.include?("#{i}")
-            matrix = ordem_colum_impares(matrix, i)
+            matrix = vetores_ordered_columns(matrix, i, ":ÍMPARES")
           elsif line.include?(":PARES") && line.include?("#{i}") 
-            matrix = ordem_colum_pares(matrix, i)
+            matrix = vetores_ordered_columns(matrix, i, ":PARES")
           elsif line.include?(":ALFABETO") && line.include?("#{i}")
-            matrix = ordem_colum_alfabetos(matrix, i)
+            matrix = vetores_ordered_columns(matrix, i, ":ALFABETO")
           end
         end
       end
